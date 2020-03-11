909d2d54493f567c16591099fd0ac894d6790afc
Tianyun Shan <nuynait@gmail.com>
2020-03-11 14:59:51 -0400

    Init

---
+++ StackViewBug/StackViewBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewBug
+//
+//  Created by Jerry Shan on 2020-03-11.
+//  Copyright © 2020 Kinetic Commerce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
